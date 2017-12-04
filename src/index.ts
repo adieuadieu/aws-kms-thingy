7201d990b24653c225fb766fa8719f711a6868db
Marco Lüthy <marco.luethy@gmail.com>
2017-12-04 17:13:44 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+export default async function decrypt(
+  ciphertext: string | ReadonlyArray<string>
+): Promise<string | ReadonlyArray<string>> {
+  if (typeof ciphertext === 'string') {
+    return kmsDecrypt(ciphertext)
+  }
+
+  return Promise.all(ciphertext.map(text => kmsDecrypt(text)))
+}
