33400beead41f98a38ef89804ca1c26073ab4429
Marco Lüthy <marco@luethy.net>
2018-02-18 22:23:41 +0100

    some cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,18 @@ import resolve from 'rollup-plugin-node-resolve'
 
 const external = ['aws-sdk', 'readline']
 
+const plugins = [
+  resolve({
+    extensions: ['.js'], // Default: ['.js']
+    jsnext: true, // Default: false
+    main: true, // Default: true
+    module: true, // Default: true
+  }),
+  // commonjs({
+  //   include: 'node_modules/**',
+  // }),
+]
+
 export default [
   {
     external,
@@ -11,44 +23,12 @@ export default [
       { file: 'dist/lib.cjs.js', format: 'cjs' },
       { file: 'dist/lib.es.js', format: 'es' },
     ],
-    plugins: [
-      resolve({
-        module: true, // Default: true
-        jsnext: true, // Default: false
-        main: true, // Default: true
-        extensions: ['.js'], // Default: ['.js']
-        // Lock the module search in this path (like a chroot). Module defined
-        // outside this path will be mark has external
-        // jail: './', // Default: '/'
-        // If true, inspect resolved files to check that they are
-        // ES2015 modules
-        // modulesOnly: true, // Default: false
-      }),
-      // commonjs({
-      //   include: 'node_modules/**',
-      // }),
-    ],
+    plugins,
   },
   {
     external,
     input: 'dist/src/cli.js',
     output: [{ file: 'dist/cli.js', format: 'cjs' }],
-    plugins: [
-      resolve({
-        module: true, // Default: true
-        jsnext: true, // Default: false
-        main: true, // Default: true
-        extensions: ['.js'], // Default: ['.js']
-        // Lock the module search in this path (like a chroot). Module defined
-        // outside this path will be mark has external
-        // jail: './', // Default: '/'
-        // If true, inspect resolved files to check that they are
-        // ES2015 modules
-        // modulesOnly: true, // Default: false
-      }),
-      // commonjs({
-      //   include: 'node_modules/**',
-      // }),
-    ],
+    plugins,
   },
 ]
