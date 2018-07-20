1c4cc8f3d21cecf31030ec0d7603a2cc39e540fe
Marco Lüthy <marco@luethy.net>
2018-07-20 22:41:02 +0200

    fix: en/decrypt function type definition issue
    The type definitions for encrypt() and decrypt() were not correct leading to type issues.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
 import kmsDecrypt from './decrypt'
 import kmsEncrypt, { InterfaceEncryptParameters } from './encrypt'
 
-export const encrypt = async (
-  parameters:
-    | InterfaceEncryptParameters
-    | ReadonlyArray<InterfaceEncryptParameters>,
-): Promise<string | ReadonlyArray<string>> =>
-  'plaintext' in parameters
+export async function encrypt(
+  parameters: InterfaceEncryptParameters,
+): Promise<string>
+export async function encrypt(
+  parameters: ReadonlyArray<InterfaceEncryptParameters>,
+): Promise<ReadonlyArray<string>>
+export async function encrypt(parameters: any): Promise<any> {
+  return 'plaintext' in parameters
     ? kmsEncrypt(parameters)
     : Promise.all(parameters.map(kmsEncrypt))
+}
 
-export const decrypt = async (
-  ciphertext: undefined | string | ReadonlyArray<string>,
-): Promise<undefined | string | ReadonlyArray<string>> =>
-  typeof ciphertext === 'undefined'
+export async function decrypt(ciphertext: undefined): Promise<undefined>
+export async function decrypt(ciphertext: string): Promise<string>
+export async function decrypt(
+  ciphertext: ReadonlyArray<string>,
+): Promise<ReadonlyArray<string>>
+export async function decrypt(ciphertext: any): Promise<any> {
+  return typeof ciphertext === 'undefined'
     ? undefined // useful in development when process.env.SECRET may be unset
     : typeof ciphertext === 'string'
       ? kmsDecrypt(ciphertext)
       : Promise.all(ciphertext.map(kmsDecrypt))
+}
